4087a4f79af0572447d82c3a18e1263876189d7b
Andreas Leicher <andreas-leicher@web.de>
2012-11-21 21:53:31 +0100

    modified product schema

--- app.js
+++ app.js
@@ -20,11 +20,15 @@ app.configure( function() {
 
 var Schema = mongoose.Schema;  
 
-var Product = new Schema({  
-    title: { type: String, required: true },  
-    description: { type: String, required: true },  
-    style: { type: String, unique: true },  
-    modified: { type: Date, default: Date.now }
+var Product = new Schema({
+	title: { type: String, required: true },
+	description: { type: String, required: true },
+	style: { type: String, unique: true },
+	images: [Images],
+	categories: [Categories],
+	catalogs: [Catalogs],
+	variants: [Variants],
+	modified: { type: Date, default: Date.now }
 });
 
 var ProductModel = mongoose.model('Product', Product);
